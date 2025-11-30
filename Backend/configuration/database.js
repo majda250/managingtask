ad4cc13cbefa349e16acaa41c0230d396a1f621d
majda250 <bendifimajda0@gmail.com>
2025-11-30 10:34:58 +0100

    Update database.js

--- Backend/configuration/database.js
+++ Backend/configuration/database.js
@@ -6,7 +6,7 @@ dotenv.config();
 const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'majda',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'taskmanaging',
   waitForConnections: true,
   connectionLimit: 10,
